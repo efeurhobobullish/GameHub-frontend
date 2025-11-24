219c829d55c903906c2ce27e5c0b0f72fe74f8fa
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-24 09:41:30 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
 import { ScrollToTop } from "@/components/ui";
 import { Home, NotFound } from "@/pages";
+import { Signup, Login } from "@/pages/auth";
 
 export default function App() {
   return (
@@ -10,6 +11,8 @@ export default function App() {
       <Toaster position="top-center" richColors />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
 
       
        
