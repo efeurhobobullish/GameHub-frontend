994f1fd243aee79958bda3c9348c5857af26757b
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-25 03:39:46 +0100

    Update index.tsx

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,6 +1,6 @@
 export { default as Dashboard } from "./dashboard";
-export { default as History } from "./History";
-export { default as Services } from "./Services";
-export { default as Wallet } from "./Wallet";
-export { default as Profile } from "./Profile";
-export { default as Support } from "./Support";
+export { default as History } from "./history";
+export { default as Services } from "./services";
+export { default as Wallet } from "./wallet";
+export { default as Profile } from "./profile";
+export { default as Support } from "./support";
